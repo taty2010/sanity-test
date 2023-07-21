6d0b10487b3d7db0c6fa42a4a05e757a9ae1e0b5
taty2010 <shydalys123@gmail.com>
2023-07-21 17:09:08 -0500

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image'
+
+import giraffe from '../public/giraffe.jpg' 
+
 export default function Custom404() {
-    return <h1>404 - Hellooo, Page Not Found</h1>
+    return (
+      <>
+        <h1>404 - Hellooo, Page Not Found</h1>
+        <Image
+          src={giraffe}
+          width={500}
+          height={500}
+          alt="Picture of the author"
+        />
+      </>
+    )
   }
